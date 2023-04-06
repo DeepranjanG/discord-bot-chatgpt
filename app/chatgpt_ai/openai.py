ad22dfd18f20c84abb01c9c0a3ba09441d9de46d
DeepranjanG <kdeepu070@gmail.com>
2023-04-06 11:30:29 +0530

    updated token

--- app/chatgpt_ai/openai.py
+++ app/chatgpt_ai/openai.py
@@ -14,7 +14,7 @@ def chatgpt_response(prompt):
         model="text-davinci-003",
         prompt=prompt,
         temperature=0,
-        max_tokens=100
+        max_tokens=250
     )
     response_dict = response.get('choices')
 
